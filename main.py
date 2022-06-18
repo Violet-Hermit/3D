6e699431b2ea25422241f7982f7d270c7ec7077d
Violet-Hermit <100461599+Violet-Hermit@users.noreply.github.com>
2022-06-18 16:00:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,98 +1,130 @@
+from functools import lru_cache
+
+#import graphics as gr
+import keyboard
+import numpy as np
+import time
+import sys
+
+from math import *
 from random import randint
 from tkinter import *
 
 
-# From Wikipedia: The Game of Life, also known simply as Life, is a cellular automaton devised by the British
-# mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its
-# initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration
-# and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing
-# machine.
-#
-# The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is
-# in one of two possible states, live or dead (or populated and unpopulated, respectively). Every cell interacts with
-# its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step
-# in time, the following transitions occur:
-#
-# 1 Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-# 2 Any live cell with two or three live neighbours lives on to the next generation.
-# 3 Any live cell with more than three live neighbours dies, as if by overpopulation.
-# 4 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-#
-# These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
-# 1 Any live cell with two or three live neighbours survives.
-# 2 Any dead cell with three live neighbours becomes a live cell.
-# 3 All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-# The initial pattern constitutes the seed of the system. The first generation is created by applying the
-# above rules simultaneously to every cell in the seed, live or dead; births and deaths occur simultaneously,
-# and the discrete moment. at which this happens is sometimes called a tick. Each generation is a pure function of the
-# preceding one. The rules continue to be applied repeatedly to create further generations.
+# Определяет поведение точкек и векторов, операции с ними
+# для класов Point и Vector есть следующие аксиомы:
+# Аксиома 1: разность между двумя точками — это вектор
+# Аксиома 2: сумма точки и вектора — это точка
+# Нужно переделывать все при помощи NumPy, но для этого нужно реализовать перегрузку конструктора,
+# чтобы все операции могли проводится как с кортежами так и с масивами NumPy
 
-class Field:
+class Point:
+    def __init__(self, x=0, y=0, z=0):
+        self.coord = (x, y, z)
 
-    def __init__(self, heigth: int, weigth: int, size_of_cell: int):
-        self.heigth = heigth
-        self.weigth = weigth
-        self.size_of_cell = size_of_cell
-        self.mtx = [[randint(0,1) for i in range(weigth)] for j in
-                    range(heigth)]  # Creation of a matrix of cells
+    def add_vector_to_point(self, vector):
+        point_x, point_y, point_z = self.coord
+        vector_x, vector_y, vector_z = vector.coord
+        result_point = Point(point_x + vector_x, point_y + vector_y, point_z + vector_z)
+        return result_point
 
-    def drawing_field(self):
-        for i in range(len(self.mtx)):
-            for j in range(len(self.mtx[i])):
-                canvas.create_rectangle(i * self.size_of_cell, j * self.size_of_cell, (i + 1) * self.size_of_cell,
-                                        (j + 1) * self.size_of_cell,
-                                        fill="black") if 1 == self.mtx[i][j] else None
+    def subtract_vector_from_point(self, vector):
+        point_x, point_y, point_z = self.coord
+        vector_x, vector_y, vector_z = vector.coord
+        result_point = Point(point_x - vector_x, point_y - vector_y, point_z - vector_z)
+        return result_point
 
-    def counting_of_neigbors(self, x, y):
+    def add_point_to_point(self, point):
+        point1_x, point1_y, point1_z = point.coord
+        point2_x, point2_y, point2_z = self.coord
+        result_vector = Vector(point2_x - point1_x, point2_y - point1_y, point2_z - point1_z)
+        return result_vector
 
-        sum: int = 0
-        left = x - 1 if x > 0 else x
-        right = x + 2 if x < len(self.mtx[y]) - 1 else x + 1
-        top = y - 1 if y > 0 else y
-        bottom = y + 2 if y < len(self.mtx) - 1 else y + 1
+    def draw_point(self):
+        x, y, z = self.coord
+        canvas.create_line(x, y, x + 1, y, fill="black")
 
-        for i in range(left, right):
-            for j in range(top, bottom):
-                sum += self.mtx[i][j]
-        return sum - self.mtx[x][y]
-
-    def cell_updating(self, x, y, new_mtx):
-
-        live = self.mtx[x][y]
-        if live == 0 and self.counting_of_neigbors(x, y) == 3:
-            new_mtx[x][y] = True
-
-        if live == 1 and self.counting_of_neigbors(x, y) == 2 or self.counting_of_neigbors(x, y) == 3:
-            new_mtx[x][y] = True
-
-        if self.counting_of_neigbors(x, y) > 3 or self.counting_of_neigbors(x, y) < 2:
-            new_mtx[x][y] = False
-
-    def clearing_screen(self):
-        canvas.create_rectangle(0, 0, (self.heigth + 1) * self.size_of_cell, (self.weigth + 1) * self.size_of_cell
-                                , fill="white")
-
-    def field_updating(self):
-        new_mtx = self.mtx.copy()
-        for i in range(len(self.mtx)):
-            for j in range(len(self.mtx[i])):
-                live = self.mtx[i][j]
-                self.cell_updating(i, j, new_mtx)
-            mtx = new_mtx
+    def move(self, x_bias, y_bias, z_bias):
+        x, y, z = self.coord
+        self.coord = (x + x_bias, y + y_bias, z + z_bias)
 
 
+class Vector:
+    def __init__(self, x=0, y=0, z=0):
+        self.coord = (x, y, z)
 
-# Main cycle
-if __name__ == '__main__':
-    size = 1000  # size of screen
+    def add_vector_to_vector(self, vector):
+        vector1_x, vector1_y, vector1_z = self.coord
+        vector2_x, vector2_y, vector2_z = vector.coord
+        result_vector = Vector(vector1_x + vector2_x, vector1_y + vector2_y, vector1_y + vector2_z)
+        return result_vector
+
+    def subtract_vector_from_vector(self, vector):
+        vector1_x, vector1_y, vector1_z = self.coord
+        vector2_x, vector2_y, vector2_z = vector.coord
+        result_vector = Vector(vector1_x - vector2_x, vector1_y - vector2_y, vector1_y - vector2_z)
+        return result_vector
+
+    def spin_xy(self, angle):
+        angle = radians(angle)
+        transformation_matrix = [[cos(angle), -1 * sin(angle), 0], [sin(angle), cos(angle), 0], [0, 0, 1]]
+        return multiply_matrices(self.coord, transformation_matrix)
+
+    def spin_yz(self, angle):
+        transformation_matrix = [[1, 0, 0], [0, cos(angle), -1 * sin(angle)], [0, sin(angle), cos(angle)]]
+        return multiply_matrices(self.coord, transformation_matrix)
+
+    def spin_xz(self, angle):
+        transformation_matrix = [[cos(angle), 0, sin(angle)], [0, 1, 0], [-1 * sin(angle), 0, cos(angle)]]
+        return multiply_matrices(self.coord, transformation_matrix)
+
+    def do_scale(self, s0, s1, s2):
+        transformation_matrix = [[s0, 0, 0], [0, s1, 0], [0, 0, s2]]
+        return multiply_matrices(self.coord, transformation_matrix)
+
+    def move(self, x_bias, y_bias, z_bias):
+        self.x += x_bias
+        self.y += y_bias
+        self.z += z_bias
+
+
+# Глобальные переменные конечно плохо, но это вроде делает код чище
+def initialize_screen(wdh, hgh):
+    global wnd
+    global canvas
     wnd = Tk()
-    canvas = Canvas(wnd, width=size, height=size)
+    canvas = Canvas(wnd, width=wdh, height=hgh)
     canvas.pack()
     colors = "black"
-    field = Field(30, 30, 20)  # creating field
-    field.drawing_field()
-    while True:
-        field.field_updating()
-        field.clearing_screen()
-        field.drawing_field()
-        wnd.update()  # update
+
+
+def clearing_screen():
+    canvas.create_rectangle(0, 0, 1000, 1000, fill="white")
+
+
+# Просто умножение матриц средствами NumPy,
+# тоже сделать прегрузку чтобы в случае подачи на вход масивов NumPy все продолжало работать
+
+def multiply_matrices(mtx1, mtx2):
+    matrix1 = np.array(mtx1)
+    matrix2 = np.array(mtx2)
+    result = matrix1.dot(matrix2).tolist()
+    return result
+
+# Утечка памяти происходит из-за модуля canvas, при каждой новой отрисовке жрётся память
+# Старые эллементы не удаляются поэтому память утекает тоже самое касается и остальных проектов кроме серпинского
+
+if __name__ == '__main__':
+    initialize_screen(1000, 1000)
+    point = Point(5, 5, 1)
+    point.draw_point()
+    points = [Point(randint(300, 700), randint(300, 700), randint(300, 700) ) for i in range(1000)]
+    for i in points:
+        i.draw_point()
+    #wnd.update()
+    while 1:
+    #clearing_screen()
+        for i in range(len(points)):
+            points[i].move(randint(-1, 1), randint(-1, 1), randint(-1, 1))
+            points[i].draw_point()
+            wnd.mainloop()
